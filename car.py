ccde0d95e7e21056546d4ffc7f1ca098b3b2c710
chiomandu <chioma.ndu@ada.ac.uk>
2025-10-23 11:31:13 +0000

    pair

--- car.py
+++ car.py
@@ -1,47 +1,54 @@
-class Car:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self._year = year
-        self._fuel = 100
+class ShoppingList():
+    def __init__(self, items = []):
+        self._items = items
+
+    def add_item(self, item):
+        self._items.append(item)
+
+    def get_num_items(self):
+        return len(self._items)
+    
+    @property
+    def list_of_items(self):
+        all_items = []
+        for item in self._items:
+            all_items.append({
+                "name": item.item_name,
+                "quantity": item.item_quantity,
+            })
+        return all_items
+    
+    def remove_item(self, item_to_remove):
+        try:
+            self._items.remove(item_to_remove)
+        except:
+            return False
+        return True
+    
+class ShoppingItem():
+    def __init__(self, item_name, item_quantity = 1):
+        self._item_name = item_name
+        self._item_quantity = item_quantity
 
     @property
-    def year(self):
-        return self._year 
-
+    def item_name(self):
+        return self._item_name
+    
     @property
-    def fuel(self):
-        return self._fuel
+    def item_quantity(self):
+        return self._item_quantity
     
-    @fuel.setter
-    def fuel(self, amount):
-        self._fuel = amount    
-    
-    
-    def set_fuel(self, amount):
-        if (self._fuel + amount) > 100:
-            self._fuel = 100
-        else:
-            self._fuel += amount
-        return f"Fuel level is now {self.fuel}"
-         
-    def drive(self):
-        self.fuel -= 10
-        return f"The {self.make} {self.model} moves forward"
-    
-    # def stop(self):
-    #     return f"The "
-    
-
-car1 = Car("Audi", "TT", 2016)
-car2 = Car("Nissan", "Micra", 2005)
-car3 = Car("Toyota", "Supra", 2010)
+    @item_quantity.setter
+    def item_quantity(self, item_quantity):
+        self._item_quantity = item_quantity
 
 
 
-car1.make = "Skoda"
-print(car1.fuel)
-#print(car1.set_fuel(50))
+apple = ShoppingItem("Apple", 47)
+orange = ShoppingItem("Orange", 67)
 
-car1.fuel = 1000
-print(car1.fuel)
+shopping_list = ShoppingList([apple,orange])
+# shopping_list.add_item(apple)
+
+shopping_list.remove_item(orange)
+print(shopping_list.list_of_items)
